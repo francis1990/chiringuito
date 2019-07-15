8d8c29958005a011f1cc177d2517ba1ca81fb8e6
jebauza <jebauza@gmail.com>
2019-07-14 21:56:41 -0400

    Cambie el ruter api fijar

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Francis pon todas tus rutas api dentro de este middleware
 Route::group(['middleware' => ['cors']], function(){ 
 
-   Route::resource('debts', 'DebtController');
+   /*Route::resource('debts', 'DebtController');
    Route::resource('payments', 'PaymentController');
    Route::resource('products', 'ProductController');
    Route::resource('purchases', 'PurchaseController');
    Route::resource('sales', 'SaleController');
-   Route::resource('sellers', 'SellerController');
+   Route::resource('sellers', 'SellerController');*/
+
+   Route::apiResource('debts', 'DebtController');
+   Route::apiResource('payments', 'PaymentController');
+   Route::apiResource('products', 'ProductController');
+   Route::apiResource('purchases', 'PurchaseController');
+   Route::apiResource('sales', 'SaleController');
+   Route::apiResource('sellers', 'SellerController');
 
 });
 
